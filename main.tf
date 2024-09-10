f6cd9c32621c45a2de14dc4d5d690c2038fc05ca
Hufaaz <ufasabdulmutolib@gmail.com>
2024-09-10 18:17:07 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,64 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_vpc" "main" {
+  default = true
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  key_name = "Ec2key"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = data.aws_vpc.main.id
+
+  tags = {
+    Name = "allow_tls"
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]    # Allow SSH from anywhere
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+output "instance_ip" {
+  value = aws_instance.web.public_ip
+}
+
