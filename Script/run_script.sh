049bbf3edb8121b93252ac367710851c36f2fdd8
Hufaaz <ufasabdulmutolib@gmail.com>
2024-09-11 13:26:55 +0100

    first commit

--- Script/run_script.sh
+++ Script/run_script.sh
@@ -1,6 +1,29 @@
-#!/bin/bash
-sudo apt update
-sudo apt install openjdk-11-jre-headless -y
-wget https://www.nextgen.com/~/media/Files/NextGen-Connect/Mirth-Connect/Mirth-Connect-Latest.tar.gz
-tar -xzf Mirth-Connect-Latest.tar.gz
-cd MirthConnect
+# Update and upgrade the system
+echo "Updating and upgrading the system..."
+sudo apt-get update && sudo apt-get upgrade -y
+
+# Install Java Runtime Environment (JRE)
+echo "Installing Java Runtime Environment..."
+sudo apt install default-jre
+
+# Download Mirth Connect
+echo "Downloading Mirth Connect..."
+curl https://s3.amazonaws.com/downloads.mirthcorp.com/connect/4.4.2.b326/mirthconnect-4.4.2.b326-unix.tar.gz -o mirthconnect.tar.gz
+
+# Extract Mirth Connect
+echo "Extracting Mirth Connect..."
+tar -xvzf mirthconnect.tar.gz
+
+# Move Mirth Connect to /opt/
+echo "Moving Mirth Connect to /opt/"
+sudo mv Mirth\ Connect/ /opt/mirthconnect
+
+# Start Mirth Connect service
+echo "Starting Mirth Connect service..."
+/opt/mirthconnect/mcservice start
+
+# Check Mirth Connect service status
+echo "Checking Mirth Connect service status..."
+/opt/mirthconnect/mcservice status
+
+echo "Installation completed!"
--- main.tf
+++ main.tf
@@ -58,6 +58,27 @@ resource "aws_security_group" "allow_tls" {
     cidr_blocks = ["0.0.0.0/0"]    # Allow SSH from anywhere
   }
 
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]    # Allow SSH from anywhere
+  }
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]    # Allow SSH from anywhere
+  }
+
+  ingress {
+    from_port = 8443
+    to_port = 8443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]    # Allow SSH from anywhere
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
